e3eae458bece9d65080ffd349ab21ee76455620b
Eddy <eddy.chi.chu@gmail.com>
2024-10-27 22:08:03 -0700

    Fix syntax mistake

--- extension_cpp/csrc/pt_mcc.cpp
+++ extension_cpp/csrc/pt_mcc.cpp
@@ -1,25 +1,27 @@
 #include <torch/script.h>
 
-namespace pt_ops
+namespace extension_cpp
 {
 
     void register_aabb(torch::Library &m);
     void register_muladd(torch::Library &m);
-    void impl_muladd(torch::Library &m)
-}
+    void impl_muladd(torch::Library &m);
 
-// Registers _C as a Python extension module.
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {}
+    // Registers _C as a Python extension module.
+    PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+    {
+    }
 
-// Defines the operators
-TORCH_LIBRARY(extension_cpp, m)
-{
-    pt_ops::register_aabb(m);
-    pt_ops::register_muladd(m);
-}
+    // Defines the operators
+    TORCH_LIBRARY(extension_cpp, m)
+    {
+        pt_ops::register_aabb(m);
+        pt_ops::register_muladd(m);
+    }
 
-// Registers CUDA implementations for mymuladd, mymul, myadd_out
-TORCH_LIBRARY_IMPL(extension_cpp, CPU, m)
-{
-    pt_ops::impl_muladd(m);
+    // Registers CUDA implementations for mymuladd, mymul, myadd_out
+    TORCH_LIBRARY_IMPL(extension_cpp, CPU, m)
+    {
+        pt_ops::impl_muladd(m);
+    }
 }
