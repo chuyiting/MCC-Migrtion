d2a414fd307e87b8d26428cd505a36e49fb66b75
Eddy <eddy.chi.chu@gmail.com>
2024-10-27 22:42:28 -0700

    Add extra test case

--- test/test_extension.py
+++ test/test_extension.py
@@ -115,6 +115,8 @@ class TestMyAddOut(TestCase):
 if __name__ == "__main__":
     pts = torch.tensor([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
     batch = torch.tensor([1, 2, 3, 4])
-    res = extension_cpp.compute_aabb(pts, batch, 4, True)
+    muladd = extension_cpp.ops.mymuladd(torch.tensor([1,2,3]), torch.tensor([1,2,3]), 4)
+    print(f'muladd result is: {muladd}')
+    res = extension_cpp.ops.compute_aabb(pts, batch, 4, True)
     print(f'the result is: {res}')
     unittest.main()
