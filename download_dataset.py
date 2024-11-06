3ca9877a20620ac3097d4e6bd6dc00dd98422e08
Eddy <eddy.chi.chu@gmail.com>
2024-11-06 00:21:04 -0800

    Fix file download error

--- download_dataset.py
+++ download_dataset.py
@@ -1,8 +1,17 @@
 import kagglehub
+import os
 
+# Specify the target directory where you want to save the dataset
 target_directory = 'data/'
 
-# Download latest version
-path = kagglehub.dataset_download("chenxaoyu/modelnet-normal-resampled", path=target_directory)
+# Ensure the target directory exists
+os.makedirs(target_directory, exist_ok=True)
 
-print("Path to dataset files:", path)
+# Download the dataset
+path = kagglehub.dataset_download("chenxaoyu/modelnet-normal-resampled")
+
+# Move the downloaded files to the target directory
+import shutil
+shutil.move(path, os.path.join(target_directory, "modelnet-normal-resampled"))
+
+print("Dataset saved at:", os.path.join(target_directory, "modelnet-normal-resampled"))
