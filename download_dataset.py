c61f58a0b8cfcb87e3dce1d648416d7676217b47
Eddy <eddy.chi.chu@gmail.com>
2024-11-06 15:17:08 -0800

    Update download script

--- download_dataset.py
+++ download_dataset.py
@@ -1,14 +1,20 @@
 import kagglehub
 import os
+import shutil
 
-# Specify the target directory where you want to save the dataset
-target_directory = 'data/'
+# # Specify the target directory where you want to save the dataset
+# target_directory = 'data/'
 
-# Ensure the target directory exists
-os.makedirs(target_directory, exist_ok=True)
+# # Ensure the target directory exists
+# os.makedirs(target_directory, exist_ok=True)
 
 # Download the dataset
 path = kagglehub.dataset_download("chenxaoyu/modelnet-normal-resampled")
 
 # Move the downloaded files to the target directory
-os.rename(path, target_directory)
+destination_dir = os.path.expanduser("~/MCC-Pytorch/data")
+
+# Move the downloaded folder to the new location and rename it to 'data'
+shutil.move(path, destination_dir)
+
+print(f"Dataset moved and renamed to {destination_dir}")
