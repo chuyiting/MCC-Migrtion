403dc4fab1c7d9d7197591ca9007fefa36f44ca1
Eddy <eddy.chi.chu@gmail.com>
2024-11-07 21:02:55 -0800

    Remove typo

--- ModelNet/ModelNet.py
+++ ModelNet/ModelNet.py
@@ -238,7 +238,7 @@ if __name__ == '__main__':
                     features = torch.from_numpy(features).float().cuda()
                     labels = torch.from_numpy(labels).long().cuda()
                     logits = model(points, batchIds, features)
-                    check_deterministic_outputs(model, points, batchIds, features):
+                    check_deterministic_outputs(model, points, batchIds, features)
                     xentropy_loss, reg_term = create_loss(logits, labels, args.weightDecay, model)
                     total_loss = xentropy_loss + reg_term
                     test_loss += total_loss.item()
