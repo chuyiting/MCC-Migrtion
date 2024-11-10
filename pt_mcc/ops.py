aedeaa587aa6c8c932a0aa1c7d92e888d324e068
Eddy <eddy.chi.chu@gmail.com>
2024-11-09 23:18:30 -0800

    Add prints

--- pt_mcc/ops.py
+++ pt_mcc/ops.py
@@ -159,19 +159,17 @@ def _spatial_conv_backward(ctx, grad):
     biases1Grads = None
     biases2Grads = None 
     biasesOutGrads = None
-    print(f'extract num of out featurres: {num_out_features}')
     
     if ctx.needs_input_grad[1]:
         featureGrads, weights1Grads, biases1Grads, weights2Grads, biases2Grads, weightsOutGrads, biasesOutGrads = \
             torch.ops.pt_mcc.spatial_conv_grad(in_points, in_features, batch_ids, in_pdfs, in_samples, start_index, packed_neigh, in_aabb_min, in_aabb_max, in_weights_hidd1, in_weights_hidd2, in_weights_out, in_bias_hidd1, in_bias_hidd2, in_bias_out, grad, num_out_features.item(), combin.item(), batch_size.item(), radius.item(), scale_inv.item(), avg.item())
     
-    print(f'feature grad shape: {featureGrads.shape}')
-    print(f'weights1 grad shape: {weights1Grads.shape}')
-    print(f'weights2 grad shape: {weights2Grads.shape}')
-    print(f'weights out grad shape: {weightsOutGrads.shape}')
-    print(f'bias1 grad shape: {biases1Grads.shape}')
-    print(f'bias2 grad shape: {biases2Grads.shape}')
-    print(f'bias out grad shape: {biasesOutGrads.shape}')
+        print(f'weights1 grad shape: {weights1Grads}')
+        print(f'weights2 grad shape: {weights2Grads}')
+        print(f'weights out grad shape: {weightsOutGrads}')
+        print(f'bias1 grad shape: {biases1Grads}')
+        print(f'bias2 grad shape: {biases2Grads}')
+        print(f'bias out grad shape: {biasesOutGrads}')
     return None, featureGrads, None, None, None, None, None, None, None, weights1Grads, weights2Grads, weightsOutGrads,biases1Grads, biases2Grads, biasesOutGrads, None, None, None, None, None, None
 
 
--- test/test_extension.py
+++ test/test_extension.py
@@ -314,9 +314,9 @@ if __name__ == "__main__":
     in_weights_hidd1 = torch.tensor([[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
                                     [0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6],
                                     [1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4]], 
-                                    device='cuda', dtype=torch.float32)
+                                    device='cuda', dtype=torch.float32).requires_grad_()
 
-    in_bias_hidd1 = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8], device='cuda', dtype=torch.float32)
+    in_bias_hidd1 = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8], device='cuda', dtype=torch.float32).requires_grad_()
 
     # Second hidden layer weights and bias
     # Shape (8, 8) for weights to satisfy in_weights_hidd2.size(0) == BLOCK_MLP_SIZE and matching in_weights_hidd1.size(1)
@@ -328,9 +328,9 @@ if __name__ == "__main__":
                                     [4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8],
                                     [4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6],
                                     [5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4]], 
-                                    device='cuda', dtype=torch.float32)
+                                    device='cuda', dtype=torch.float32).requires_grad_()
 
-    in_bias_hidd2 = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8], device='cuda', dtype=torch.float32)
+    in_bias_hidd2 = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8], device='cuda', dtype=torch.float32).requires_grad_()
 
     # Output layer weights and bias
     # Shape (8, 8) for weights to match BLOCK_MLP_SIZE and also ensure compatibility with in_weights_hidd2 size
@@ -342,9 +342,9 @@ if __name__ == "__main__":
                                 [4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8],
                                 [4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6],
                                 [5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4]], 
-                                device='cuda', dtype=torch.float32)
+                                device='cuda', dtype=torch.float32).requires_grad_()
 
-    in_bias_out = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8], device='cuda', dtype=torch.float32)
+    in_bias_out = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8], device='cuda', dtype=torch.float32).requires_grad_()
 
 
     # Scalar tensors
